34af6643162e3257b66673efb152f9e99be8d280
pabpereza <pabloperezaradros@gmail.com>
2022-04-12 18:49:26 +0200

    Added sender script

---
+++ service-controller/sender.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO           # import RPi.GPIO module  
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)          # choose BCM or BOARD  
+
+# Pin control
+GPIO.setup(4, GPIO.OUT) 
+
+GPIO.output(7, True)
+
+sleep(1)
+
+
+GPIO.output(7, False)
