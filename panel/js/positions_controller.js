8838dfbbc4eeffdcccea0bb96a41073c8eadf995
Fermin <fjimenez@ginzo.tech>
2022-05-26 17:42:48 +0200

    update program

--- panel/js/positions_controller.js
+++ panel/js/positions_controller.js
@@ -2,58 +2,98 @@
 var url = 'http://localhost:8080/';
 
 
-function refresh_positions(){
+function refresh_positions() {
 
-	$.getJSON( url + 'positions', { get_param: 'value' }, function(json) {
-		
-		for( positions in json){
-			position = json[positions];
-			
-			index = position['position']
-			active = position['active']
-			path = position['path']
-			
-			if(active == true){
-				$('#btn_puesto_'+index).removeClass('btn-danger');
-				$('#btn_puesto_'+index).addClass('btn-success');
-			}else
-			{
-				$('#btn_puesto_'+index).removeClass('btn-success');
-				$('#btn_puesto_'+index).addClass('btn-danger');
-			}
-		}
-		
-	});
+    $.getJSON(url + 'positions', {get_param: 'value'}, function (json) {
+
+        for (positions in json) {
+            position = json[positions];
+
+            index = position['position']
+            active = position['active']
+            path = position['path']
+
+            if (active == true) {
+                $('#btn_puesto_' + index).removeClass('btn-danger');
+                $('#btn_puesto_' + index).addClass('btn-success');
+            } else {
+                $('#btn_puesto_' + index).removeClass('btn-success');
+                $('#btn_puesto_' + index).addClass('btn-danger');
+            }
+        }
+
+    });
 
 }
 
 
-function change_active_position(position){
-	$.ajax(url + "positions", {
-		data: JSON.stringify({'position': position }),
-		method: "POST",
-		contentType: "application/json",
-		success: function(result) {
-			refresh_positions();
-		}
-	});
+function change_active_position(position) {
+    $.ajax(url + "positions", {
+        data: JSON.stringify({'position': position}),
+        method: "POST",
+        contentType: "application/json",
+        success: function (result) {
+            refresh_positions();
+        }
+    });
 
 }
 
 
 // MAIN
 // First refresh of the positions
-refresh_positions()
+refresh_positions();
 
+// PROGRAMS
+var gcode_programs = {};
+$.ajax(url + "paths", {
+    method: "GET",
+    contentType: "application/json",
+    success: function (result) {
+        gcode_programs = result;
+    }
+});
+
+// END PROGRAMS
+
+// EVENTS
 // Add event listener to the buttons
-$('[id*="btn_puesto_"').click(function(){
-	change_active_position(this.id.split('_')[2]);
+$('[id*="btn_puesto_"]').click(function () {
+    change_active_position(this.id.split('_')[2]);
 });
 
 // Add event listener to the change program buttons
-$('[id*="btn_program_"').click(function(){
-	let id_position = $( this ).data("position");
-	$('#modal_input_position').val(id_position);
-	$('#modal_span_position').text(id_position);
-	$('#myModal').modal('show');
+$('[id*="btn_program_"]').click(function () {
+    let id_position = $(this).data("position");
+    $('#modal_input_position').val(id_position);
+    $('#modal_span_position').text(id_position);
+
+    // Programs
+    let modal_select_program = $('#modal_select_program');
+    modal_select_program.empty();
+    modal_select_program.append(new Option('Selecciona el programa', ''));
+
+    for (let program of gcode_programs) {
+        modal_select_program.append($('<option>', {
+            value: program,
+            text: program
+        }));
+    }
+    $('#myModal').modal('show');
+});
+
+$('[id*="modal_btn_send"]').click(function () {
+    // Update and refresh
+    let position = $('#modal_input_position').val();
+    let path = $('#modal_select_program').val();
+
+    $.ajax(url + "position/path", {
+        data: JSON.stringify({'position': position, 'path': path}),
+        method: "UPDATE",
+        contentType: "application/json",
+        success: function (result) {
+            console.log(result);
+            refresh_positions();
+        }
+    });
 });
--- service_controller/controller.py
+++ service_controller/controller.py
@@ -6,126 +6,110 @@ import glob
 
 global_dir = '.'
 
+
 def setGlobalDir(direction):
-	global global_dir
-	global_dir = direction 
+    global global_dir
+    global_dir = direction
 
 
 def seta():
-	print("Pulsada la seta")
-	sendGCode(0)
+    print("Pulsada la seta")
+    sendGCode(0)
 
 
 def stop():
-	print("Stopping gpio-lector service")
-	os.system("systemctl stop gpio-lector")
+    print("Stopping gpio-lector service")
+    os.system("systemctl stop gpio-lector")
 
 
 def restart():
-	print("Restarting gpio-lector service")
-	os.system("systemctl restart gpio-lector")
+    print("Restarting gpio-lector service")
+    os.system("systemctl restart gpio-lector")
 
 
 def status():
-	print("Status of gpio-lector service")
-	os.system("systemctl status gpio-lector") 
+    print("Status of gpio-lector service")
+    os.system("systemctl status gpio-lector")
 
 
 def getPositions():
-	with open( global_dir + '/service_controller/positions.json') as json_file:
-		data = json.load(json_file)
-		return json.dumps(data)
+    with open(global_dir + '/service_controller/positions.json') as json_file:
+        data = json.load(json_file)
+        return json.dumps(data)
 
 
-def setPosition( position):
-	data = ""
-	json_file = open(global_dir+ '/service_controller/positions.json')
-	data = json.load(json_file)
-	json_file.close()
+def setPosition(position):
+    json_file = open(global_dir + '/service_controller/positions.json')
+    data = json.load(json_file)
+    json_file.close()
 
-	data[position-1]['active'] = !data[position-1]['active']
-	
-	if not data == "":
-		json_file = open(global_dir+ '/service_controller/positions.json','w')
-		json.dump(data, json_file)
-		json_file.close()
-		return "OK"
+    data[position - 1]['active'] = not data[position - 1]['active']
 
-	return "ERROR"
+    if not data == "":
+        json_file = open(global_dir + '/service_controller/positions.json', 'w')
+        json.dump(data, json_file)
+        json_file.close()
+        return "OK"
+
+    return "ERROR"
+
 
 def updatePositionPath(position, path):
-	data = ""
-	json_file = open(global_dir+ '/service_controller/positions.json')
-	data = json.load(json_file)
-	json_file.close()
+    json_file = open(global_dir + '/service_controller/positions.json')
+    data = json.load(json_file)
+    json_file.close()
 
-	data[position-1]['path'] = path
-	
-	if not data == "":
-		json_file = open(global_dir+ '/service_controller/positions.json','w')
-		json.dump(data, json_file)
-		json_file.close()
-		return "OK"
+    data[position - 1]['path'] = path
 
-	return "ERROR"
+    if not data == "":
+        json_file = open(global_dir + '/service_controller/positions.json', 'w')
+        json.dump(data, json_file)
+        json_file.close()
+        return "OK"
+
+    return "ERROR"
 
 
 def getPaths():
-	'''
-	Get all paths from paths.json
-	'''
-	# TODO: modify ./ , use global_dir
-	base = global_dir + '/service_controller/'
-	files = glob.glob(base + 'gcodes/**/*.gcode', recursive=True)
-	files = list(map(lambda x : x.replace(base, ''), files))
-	return files
-
-def setPath(path):
-	data = ""
-	json_file = open(global_dir+ '/service_controller/positions.json')
-	data = json.load(json_file)
-	json_file.close()
-
-	data[position-1]['path'] = False
-	
-	if not data == "":
-		json_file = open(global_dir+ '/service_controller/positions.json','w')
-		json.dump(data, json_file)
-		json_file.close()
-		return "OK"
-
-	return "ERROR"
+    '''
+    Get all paths from paths.json
+    '''
+    # TODO: modify ./ , use global_dir
+    base = global_dir + '/service_controller/'
+    files = glob.glob(base + 'gcodes/**/*.gcode', recursive=True)
+    files = list(map(lambda x: x.replace(base, ''), files))
+    return files
 
 
 def getCodes():
-	f = []
-	for (dirpath, dirnames, filenames) in os.walk( global_dir + '/GCodes' ):
-		print(  dirnames, filenames)
+    f = []
+    for (dirpath, dirnames, filenames) in os.walk(global_dir + '/GCodes'):
+        print(dirnames, filenames)
 
-	return "OK"
+    return "OK"
 
-def sendGCode( puesto ):
 
-	if puesto == 0:
-		print("Send reset program")
-		return "RESET"
-	elif checkIfPositionIsActive(puesto):
-		path = searchPath(puesto)
-		print("Send GCode from: " + path + " to position: " + str(puesto))
-		return sender.sendGCode( path )
+def sendGCode(puesto):
+    if puesto == 0:
+        print("Send reset program")
+        return "RESET"
+    elif checkIfPositionIsActive(puesto):
+        path = searchPath(puesto)
+        print("Send GCode from: " + path + " to position: " + str(puesto))
+        return sender.sendGCode(path)
 
-	return "El puesto seleccionado no esta activo\n"
+    return "El puesto seleccionado no esta activo\n"
+
 
 ## AUXILIAR FUNCTIONS ##
 ## -------------------- ##
-def searchPath( puesto):
-	with open(global_dir + '/service_controller/positions.json') as json_file:
-		data = json.load(json_file)
-		return data[puesto-1]['path']
+def searchPath(puesto):
+    with open(global_dir + '/service_controller/positions.json') as json_file:
+        data = json.load(json_file)
+        return data[puesto - 1]['path']
 
 
 def checkIfPositionIsActive(puesto):
-	with open(global_dir + '/service_controller/positions.json') as json_file:
-		data = json.load(json_file)
-		return data[puesto-1]['active']
-
+    with open(global_dir + '/service_controller/positions.json') as json_file:
+        data = json.load(json_file)
+        return data[puesto - 1]['active']
