9300bb04be443b4384b39dad0f959db63ced4518
Fermin <fjimenez@ginzo.tech>
2022-05-27 18:23:13 +0200

    get program

--- service_controller/g_code_sender.py
+++ service_controller/g_code_sender.py
@@ -1,62 +1,66 @@
 #!/usr/bin/python
 """\
 Simple g-code streaming script
-""" 
+"""
+import logging
+
 from serial import Serial
 import time
 import argparse
 
+logging.basicConfig(filename='service.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-def sendGCode( path):
 
+def sendGCode(path):
     # show values ##
     # print ("USB Port: %s" % args.port )
-    print ("Gcode file: %s" % path )
+    print("Gcode file: %s" % path)
 
     # Open serial port
-    #s = serial.Serial('/dev/ttyACM0',115200)
+    # s = serial.Serial('/dev/ttyACM0',115200)
     s = ""
 
     try:
-        s = Serial('/dev/ttyACM0',115200)
+        s = Serial('/dev/ttyACM0', 115200)
         print('Opening Serial Port')
     except:
         return "Encoladora no conectada \n"
 
     try:
         # Open g-code file
-        f = open(args.file,'r');
-        #f = open(args.file,'r');
-        print('Opening gcode file')
-        
+        f = open(path, 'r')
+        # f = open(args.file,'r');
+        logging.info("Reading file " + str(path))
+
         # Wake up 
-        s.write("\r\n\r\n") # Hit enter a few times to wake the Printrbot
-        time.sleep(2)   # Wait for Printrbot to initialize
+        s.write("\r\n\r\n")  # Hit enter a few times to wake the Printrbot
+        time.sleep(2)  # Wait for Printrbot to initialize
         s.flushInput()  # Flush startup text in serial input
-        print('Sending gcode')
-        
+
+        logging.info("Start - sending gcode")
         # Stream g-code
         for line in f:
             l = removeComment(line)
-            l = l.strip() # Strip all EOL characters for streaming
-            if  (l.isspace()==False and len(l)>0) :
+            l = l.strip()  # Strip all EOL characters for streaming
+            if (l.isspace() == False and len(l) > 0):
                 print('Sending: ' + l)
-                s.write(l + '\n') # Send g-code block
-                grbl_out = s.readline() # Wait for response with carriage return
-                print(' : ' + grbl_out.strip())
-        
+                s.write(l + '\n')  # Send g-code block
+                grbl_out = s.readline()  # Wait for response with carriage return
+                print(' : ' + str(grbl_out).strip())
 
+        logging.info("End - sending gcode")
         # Close file and serial port
         f.close()
         s.close()
     except:
+        logging.error("Error gcode " + str(path))
         return "Error en el puerto serie"
 
     return "OK"
 
 
 def removeComment(string):
-    if (string.find(';')==-1):
+    if (string.find(';') == -1):
         return string
     else:
         return string[:string.index(';')]
